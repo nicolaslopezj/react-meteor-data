b1b8e9e5cce9a0dcb20f8de86fa377f3a6286687
Joaquin Gumucio L <jjgumucio@gmail.com>
2016-10-20 11:02:58 -0300

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'orionsoft:react-meteor-data',
-  version: '0.1.2',
+  version: '0.1.3',
   summary: 'Fetch Meteor data in React using decorators',
   git: 'https://github.com/orionsoft/react-meteor-data',
   documentation: 'README.md'
